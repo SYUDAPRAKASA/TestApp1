7c5c2ced9ca5b8058c139603645564985642a213
unknown <septianprakasa@gmail.com>
2020-02-23 22:11:59 +0700

    AddScope IAuthRepository and AuthRepository

--- TestApp.API/Startup.cs
+++ TestApp.API/Startup.cs
@@ -29,6 +29,8 @@ namespace TestApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
+            services.AddScoped<IAuthRepository,AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
